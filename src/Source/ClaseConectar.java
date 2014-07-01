73bb5f7372b175da0ea93c2689cf9b3c7113ec4d
KaiserFrederick <CodeFluid@hotmail.com>
2014-07-01 18:59:56 -0500

    Create ClaseConectar.java
    Esta clase muestra como hacer la conexión a MySQL.

---
+++ src/Source/ClaseConectar.java
@@ -0,0 +1,28 @@
+package Source;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+/**
+ *
+ * @author Unknown
+ */
+public class ClaseConectar { 
+
+    private Connection conn;
+    
+    public Connection conectar(){
+        try {
+            Class.forName("com.mysql.jdbc.Driver");
+            conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/dbche", "root", "Dakaris7");
+        } catch (ClassNotFoundException | SQLException e) {
+            System.out.println("Error: " + e.getMessage());
+        }
+        return conn;
+    }
+    
+    public void cerrar() throws SQLException{
+        conectar().close();
+    }
+}
